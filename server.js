ee4dc755cbd435bfb90a3454ed86f37539047774
Volodymyr Liebied <volodymyr.liebied@gmail.com>
2016-08-20 21:33:53 +0300

    Added logging and foreman

--- server.js
+++ server.js
@@ -5,13 +5,26 @@ var port     = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
 var logger   = require('morgan');
 var path = require('path');
-
+var fs = require('fs');
 
 // prepare server
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger('dev')); // log every request to the console
+
+
+var accessLogStream = fs.createWriteStream(__dirname + '/log/access.log',{flags: 'a'});
+
+console.info('env: ' + app.get('env'));
+console.info('APP_ENV: ' + app.get('APP_ENV'));
+console.info('ENV: ' + app.get('ENV'));
+
+if (app.get('env') == 'production') {
+  app.use(logger('combined', { /*skip: function(req, res) { return res.statusCode < 400 },*/ stream: accessLogStream }));
+} else {
+  app.use(logger('dev'));
+}
 
 var api = require('./api')
 app.use('/api/v1', api); // redirect API calls
@@ -26,3 +39,4 @@ app.get('/', function (req, res) {
 }); // initial HTML file
 
 app.listen(port);
+console.info('Server listening on port: ' + port);
